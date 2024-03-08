a156690e18cbda819f37dc8ea1935d2cfd0880b2
vbeemacherla <vbeemacherla@IN-6KSQ8S3>
2024-03-08 12:58:51 +0530

    Add project files.

---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[3,5];
+            a[]
+
+
+        }
+    }
+}
